3068d14975bf3faf937185905f4c2dbe467619f5
zelihaarduclu <61326828+zelihaarduclu@users.noreply.github.com>
2021-02-26 14:46:30 +0200

    ilkk

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        /* MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.*/
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad +" "+ "eklendı");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+musteri.Soyad+"silindi");
+        }
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad+"listelendi");
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bir bankada müşteri takibi yapmak istiyorsunuz.
+            Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
+            MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.*/
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "zeliha";
+            musteri1.Soyad = "arduçlu";
+
+            Musteri[] musteris = new Musteri[] { musteri1 };
+            foreach (var musteri2 in musteris)
+            {
+                Console.WriteLine(musteri2.Ad+" "+musteri2.Soyad);
+            }
+            //metodları burada kullanmadım ama belkı daha kapsamlı uygulamalarda kullanırım ama su an program.cs de kullanmıycam 
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+        }
+    }
+}
